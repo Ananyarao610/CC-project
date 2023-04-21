620522d7add22f4aeea42bc086382ecd4a07e404
Ananya Rao <ananyarao610@gmail.com>
2023-04-21 22:46:58 +0530

    added files

---
+++ src/main/java/com/mycompany/taskManager.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class taskManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(taskManager.class, args);
+    }
+
+}
